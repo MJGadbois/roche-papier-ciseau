1f858575fe636d90787bbff0b77eb8249e1c1562
MJGadbois <116755684+MJGadbois@users.noreply.github.com>
2022-11-04 09:07:07 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onGesture(Gesture.Shake, function () {
+    if (randint(1, 3) == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (randint(1, 3) == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . # #
+            . # . . #
+            . . # # .
+            . # . . #
+            # . . # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
